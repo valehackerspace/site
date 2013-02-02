35aa09903af49459368a0091591f0f8e1637b875
Marcelo Tambalo <marcelo.zokis@gmail.com>
2013-02-02 21:05:10 -0200

    ref #2

--- sitehsv/sistema/models.py
+++ sitehsv/sistema/models.py
@@ -1,3 +1,9 @@
+# coding: utf-8
 from django.db import models
 
-# Create your models here.
+
+class Projetos(models.Model):
+    nome = models.CharField(u'Projeto', max_length=50, blank=False, null=False)
+    descricao = models.TextField(u'Descrição', blank=False, null=False)
+    inicio = models.DateField(label=u'Data de Início', auto_now_add=True)
+    previsao_termino = models.DateField(label=u'Previsão de Término')
