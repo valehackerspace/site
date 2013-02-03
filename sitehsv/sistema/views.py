14660263a8442f1154bf1beb83a74191399a8e7b
Marcelo Tambalo <marcelo.zokis@gmail.com>
2013-02-03 16:14:55 -0200

    definindo views iniciais

--- sitehsv/sistema/views.py
+++ sitehsv/sistema/views.py
@@ -3,4 +3,24 @@ from django.http import Http404, HttpResponse
 
 
 def home(request):
-    return HttpResponse('A')
+    return HttpResponse('Home')
+
+
+def eventos_list(request):
+    return HttpResponse('Eventos')
+
+
+def projetos_list(request):
+    return HttpResponse('Projetos')
+
+
+def post(request, pk=None):
+    return HttpResponse('Post')
+
+
+def evento(request, pk=None):
+    return HttpResponse('Evento')
+
+
+def projeto(request, pk=None):
+    return HttpResponse('Projeto')
